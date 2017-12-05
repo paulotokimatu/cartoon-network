e1cae64affed53e8da6e1550e130b4bb564bad9a
paulotokimatu <paulomtf2005@yahoo.com.br>
2017-12-05 17:29:50 -0200

    Added tests to cartoon-list component.

--- src/app/main-content/cartoon-list/cartoon-list.component.spec.ts
+++ src/app/main-content/cartoon-list/cartoon-list.component.spec.ts
@@ -25,4 +25,68 @@ describe('CartoonListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get details of the schedule of a cartoon', () => {
+    let service = new CartoonListService();
+    spyOn(service, 'getOneCartoonSchedule').and.returnValue({titlePt: 'Apenas Um Show'});
+    
+    component.onOpenCartoonDetailsModal('regularShow');
+    expect(component.selectedCartoon.titlePt).toBe('Apenas Um Show');
+  });
+
+  it('should not allow startIndex to go under zero', () => {
+    let mockCartoonsFeatured = [
+      'regularShow',
+      'jorelsBrother',
+      'thePowerpuffGirls',
+      'tomAndJerry',
+      'dragonBall',
+      'scoobyDoo',
+    ];
+
+    component.featuredCartoons = mockCartoonsFeatured;
+    
+    component.onChangeVisibleCartoons(-1);
+    component.onChangeVisibleCartoons(-1);
+    component.onChangeVisibleCartoons(-1);
+
+    expect(component.startIndex).toBe(0);
+  });
+
+  it('should not allow startIndex to be larger than list length - maxVisibleFeaturedCartoons', () => {
+    let mockCartoonsFeatured = [
+      'regularShow',
+      'jorelsBrother',
+      'thePowerpuffGirls',
+      'tomAndJerry',
+      'dragonBall',
+      'scoobyDoo',
+    ];
+
+    component.featuredCartoons = mockCartoonsFeatured;
+
+    component.onChangeVisibleCartoons(1);
+    component.onChangeVisibleCartoons(1);
+    component.onChangeVisibleCartoons(1);
+
+    expect(component.startIndex).toBe(1);
+  });
+
+  it('should not change startIndex if list length < maxVisibleFeaturedCartoons', () => {
+    let mockCartoonsFeatured = [
+      'regularShow',
+      'jorelsBrother',
+      'thePowerpuffGirls',
+      'tomAndJerry',
+    ];
+
+    component.featuredCartoons = mockCartoonsFeatured;
+
+    component.onChangeVisibleCartoons(-1);
+    component.onChangeVisibleCartoons(-1);
+    expect(component.startIndex).toBe(0);
+    
+    component.onChangeVisibleCartoons(1);
+    expect(component.startIndex).toBe(0);
+  });
 });
